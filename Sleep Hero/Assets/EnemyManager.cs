abf578decdd77a1390c54be0cf16ea3a91b95f35
Vinicius Hashirama <vinicius_freires@hotmail.com>
2019-08-03 02:54:48 -0300

    Enemy Damage Taken

---
+++ Sleep Hero/Assets/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int health;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage){
+        health -= damage;
+        Debug.Log("Damage Taken");
+
+    }
+}
---
+++ Sleep Hero/Assets/PlayerAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+   
+   public float timeBtwAttack;
+   public float startTimeBtwAttack;
+
+   public Transform attackpos;
+   public LayerMask whatIsEnemy;
+   public float attackRange;
+
+   public int damage;
+
+   private void Update() {
+       if( timeBtwAttack <= 0){
+           if(Input.GetKeyDown(KeyCode.LeftShift)){
+
+               Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackpos.position, attackRange,whatIsEnemy );
+
+               for (int i = 0; i < enemiesToDamage.Length; i++)
+               {
+                   enemiesToDamage[i].GetComponent<EnemyManager>().TakeDamage(damage);
+               }
+
+               Debug.Log("Attacking");
+           }
+
+           timeBtwAttack = startTimeBtwAttack;
+       }else{
+           timeBtwAttack -= Time.deltaTime;
+       }
+   }
+
+
+
+   private void OnDrawGizmosSelected() {
+       
+   
+       Gizmos.color = Color.red;
+       Gizmos.DrawWireSphere(attackpos.position,attackRange);
+   }
+}
