a9655db459490dfd4e294cbf5cd02b87bad9c33d
Vinicius Hashirama <vinicius_freires@hotmail.com>
2019-08-03 01:32:49 -0300

    Initial Commit Project

---
+++ Sleep Hero/Assets/_Project/Scripts/PlayerMovement/PlayerInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+
+    public float horizontal;
+    public float vertical;
+
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+
+    Vector2 vectorHorizontalmovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        UpdatePlayerMovement();
+
+
+    }
+
+
+
+
+    public void UpdateInputs(){
+
+
+    }
+
+    public void UpdatePlayerMovement(){
+
+        Vector2 movHorizontal;
+        movHorizontal = transform.right * horizontal;
+
+        vectorHorizontalmovement = movHorizontal * speed;
+
+        if(vectorHorizontalmovement != Vector2.zero){
+            
+            Vector2 pos  = new Vector2(transform.position.x, transform.position.y) + vectorHorizontalmovement * Time.fixedDeltaTime;
+            rb.MovePosition(pos);
+        }
+    }
+
+    
+}
