7b5154d059c90540f79d7519be39ca8d5807c2f8
Vinicius Hashirama <vinicius_freires@hotmail.com>
2019-08-03 17:26:42 -0300

    included Player Cicle

---
+++ Sleep Hero/Assets/GameManagerGeral.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManagerGeral : MonoBehaviour
+{   
+    [Header("References")]
+    public UIManager uIManager;
+
+    [Header("Debug Variables")]
+    public bool isPlaying;
+    public float actualSleep;
+    public float maxSleep;
+    public float sleepLoseAmount;
+
+
+
+    public static GameManagerGeral instance;
+
+
+    
+
+    
+    [Header("Settings")]
+    public float startSleep =100f;
+
+    //public int level;
+    public float[] Difficulty;
+
+
+    public float interval;
+    
+
+
+private void Awake() {
+    if(instance == null)
+     instance = this;
+}
+
+private void Start() {
+    ResetGame();
+    StartGame();
+}
+
+
+    private void Update() {
+
+        if(Input.GetKeyDown(KeyCode.R))
+            ResetGame();
+
+        if(Input.GetKeyDown(KeyCode.T))
+            StartGame();
+
+        if(Input.GetKeyDown(KeyCode.Y))
+            StopGame();
+
+    }
+
+
+public    void StartGame(){
+
+        isPlaying = true;
+        StartCoroutine(SleepRoutine());
+    
+    }
+
+    
+    
+    public void StopGame(){
+        isPlaying = false;
+        SceneManager.LoadScene("SampleScene");
+
+    }   
+
+    
+    public void ResetGame(){
+
+        actualSleep = startSleep;
+        SetDifficulty(1);
+        
+    }
+
+
+    IEnumerator SleepRoutine(){
+
+        while (isPlaying)
+        {
+            actualSleep -= sleepLoseAmount;
+            uIManager.RefreshUI();
+            yield return new WaitForSeconds(interval);
+
+
+        }
+    }
+
+
+    void SetDifficulty(int level){
+       sleepLoseAmount = Difficulty[level];
+
+    }
+
+    public void AddSleepReward(float amount){
+        actualSleep +=amount;
+    }
+
+
+
+}
